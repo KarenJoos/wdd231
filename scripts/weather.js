cbdfffc60cb421117aaa61d3eea3e75e0997e392
KarenJoos <karen.karebear@gmail.com>
2025-07-17 20:20:34 -0600

    updates

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,4 +4,6 @@ const captionDesc = document.querySelector('figcaption');
 
 // 49.75, 6.63
 
+const myKey = 'https://github.com/KarenJoos/wdd231.git'
+
 const url = 'api.openweathermap.org/data/2.5/forecast?lat={49.75}&lon={6.63}&appid={63b2bb263f9f2fa71a0b7cba76257361}'
