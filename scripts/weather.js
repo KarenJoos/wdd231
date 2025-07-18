760e8e6d3f95e793be413521453d6fe87d1ce5b7
KarenJoos <karen.karebear@gmail.com>
2025-07-17 19:41:44 -0600

    directory facebook meta data

---
+++ scripts/weather.js
@@ -0,0 +1,7 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+// 49.75, 6.63
+
+const url = 'api.openweathermap.org/data/2.5/forecast?lat={49.75}&lon={6.63}&appid={63b2bb263f9f2fa71a0b7cba76257361}'
