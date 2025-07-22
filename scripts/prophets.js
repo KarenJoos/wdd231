cad87654f8a3cd81ff07267cd1bb98c302ba9e46
KarenJoos <karen.karebear@gmail.com>
2025-07-21 23:22:03 -0600

    prophets update

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -5,7 +5,32 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch('https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json');
     const data = await response.json();
-    console.table(data.prophets);
+    // console.table(data.prophets); 
+    displayProphets(data.prophets);
+
+    const displayProphets = (prophets) => {
+        prophets.forEach((prophet) => {
+
+            let card = document.createElement('section');
+            letfullName = document.createElement('h2');
+            let img = document.createElement('portrait');
+
+            fullName.textContent = `${prophet.name} lastname`;
+
+            portrait.setAttribute('src', prophet.imageurl);
+            portrait.setAttribute('alt', `Portrait of ${prophet.name} lastname`);
+            portrait.setAttribute('loading', 'lazy');
+            portrait.setAttribute('width', '340');
+            portrait.setAttribute('height', '440');
+
+            card.appendChild(card);
+            card.appendChild(portrait);
+
+            cards.appendChild(card);
+        });
+    }
 }
+
+
 
 getProphetData();
