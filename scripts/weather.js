c4b5832dea1fdbf76c1e177d994b24422a758271
KarenJoos <karen.karebear@gmail.com>
2025-07-29 14:47:38 -0600

    weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -4,6 +4,34 @@ const captionDesc = document.querySelector('figcaption');
 
 // 49.75, 6.63
 
-const myKey = 'https://github.com/KarenJoos/wdd231.git'
+const myKey = "2ae97b4f41228da1a40cc7e460e7379a";
+const myLat = "49.75";
+const myLong = "6.63";
 
-const url = 'api.openweathermap.org/data/2.5/forecast?lat={49.75}&lon={6.63}&appid={63b2bb263f9f2fa71a0b7cba76257361}'
+const url = `//api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            // displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].ico}.@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('SRC', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
