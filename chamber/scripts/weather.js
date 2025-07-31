e8e65976086b576bcfafb0650f2afbc735490f2a
KarenJoos <karen.karebear@gmail.com>
2025-07-31 14:57:51 -0600

    updates

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,12 +1,18 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const captionDesc = document.querySelector('#description');
+const tempHigh = document.querySelector('#high');
+const tempLow = document.querySelector('#low');
+const humidity = document.querySelector('#humidity');
+const sunrise = document.querySelector('#sunrise');
+const sunset = document.querySelector('#sunset');
+
 
 // 40.51, -112.03
 
 const myKey = "2ae97b4f41228da1a40cc7e460e7379a";
-// const myLat = "49.75";
-// const myLong = "6.63";
+// const myLat = "40.51";
+// const myLong = "-112.03";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=40.51&lon=-112.03&appid=${myKey}`;
 
@@ -26,12 +32,15 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    let fahrenheitTemp = `${(data.main.temp - 273.15) * 1.8 + 32}`;
+    currentTemp.innerHTML = fahrenheitTemp.toString(2) + '&deg;F';
+    humidity.innerHTML = `${data.main.humidity}%`;
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('SRC', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc}`;
+    // tempHigh.textContent = `${hi}`;
 }
 
 apiFetch();
--- startLabTemple/js/scripts.js
+++ startLabTemple/js/scripts.js
@@ -1,8 +1,8 @@
-import { temples } from '../data/temples.js';
-console.log(temples);
+import { temples } from '../data/temples.js'
+// console.log(temples)
 
-import { url } from '../data/temples.js';
-console.log(url);
+import { url } from '../data/temples.js'
+// console.log(url)
 
 // grab a reference to the division where we display the items
 const showHere = document.querySelector('#showHere');
@@ -12,21 +12,29 @@ const mytitle = document.querySelector('#mydialog h2');
 const myinfo = document.querySelector('#mydialog p');
 const myclose = document.querySelector('#mydialog button');
 
-myclose.addEventListener("click", () => {
-    mydialog.closest();
-});
+myclose.addEventListener("click", () => mydialog.close());
 
 // loop through the array of JSON items
 function displayItems(data) {
     console.log(data);
     data.forEach(x => {
         console.log(x)
-    })
-}
+        const photo = document.createElement('img')
+        photo.src = `${url}${x.path}`
+        photo.alt = x.name
+        
+        photo.addEventListener('click', () => showStuff(x));
+        showHere.appendChild(photo)
+    }) //end loop
+} //end function
 
 displayItems(temples);
 
-// name
-// dedicated
-// person
-// number
+
+// Populate the dialog with information when image is clicked
+function showStuff(x) {
+    mytitle.innerHTML = x.name
+    mydialog.showModal()
+    myinfo.innerHTML = `Dedicated ${x.dedicated} by ${x.person} as temple number ${x.number}`
+} //end of function
+
