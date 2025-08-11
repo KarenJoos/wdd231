84cead320b03d1a8483f7a5f330374f0c6c69204
KarenJoos <karen.karebear@gmail.com>
2025-08-11 14:48:21 -0600

    final started

---
+++ final/scripts/dates.js
@@ -0,0 +1,6 @@
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
+
+const d = new Date();
+let currentyear = d.getFullYear();
+document.getElementById('currentyear').innerHTML = currentyear;
