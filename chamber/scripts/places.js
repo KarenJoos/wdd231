4e60f8abbf94fbf08406f2891ab6e22faa9e8009
KarenJoos <karen.karebear@gmail.com>
2025-08-12 22:41:00 -0600

    updates

--- chamber/scripts/places.js
+++ chamber/scripts/places.js
@@ -1,7 +1,7 @@
 import {places} from '../data/places.mjs'
 console.log(places);
 
-const showHere = document.querySelector('#allplaces');
+const showHere = document.querySelector('#allPlaces');
 
 function displayItems(places) {
     places.forEach(x => {
@@ -24,6 +24,14 @@ function displayItems(places) {
         const theDesc = document.createElement('p')
         theDesc.innerText = x.description
         theCard.appendChild(theDesc)
+        //build the learn more button
+        const learnbtn = document.createElement('button')
+        learnbtn.innerHTML = `Learn More`
+
+        learnbtn.addEventListener('click', () => {
+            learnbtn.innerText = x.cost
+        })
+        theCard.appendChild(learnbtn)
 
         showHere.appendChild(theCard)
     }); //end loop
@@ -31,3 +39,30 @@ function displayItems(places) {
 
 //Start displaying all items in the JSON file
 displayItems(places)
+
+function updateSidebarMessage() {
+    const sidebar = document.querySelector('#sidebar');
+    const lastVisit = localStorage.getItem('lastVisit');
+    const currentDate = new Date();
+
+    if (!lastVisit) {
+        //First visit
+        sidebar.innerText = 'Welcome! Let us know if you have any questions.';
+    } else {
+        const lastVisitDate = Date.parse(lastVisit);
+        const diffInMilliseconds = currentDate - lastVisitDate;
+        const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24)); //converts to days
+
+        if (diffInDays < 1) {
+            sidebar.innerText = 'Back so soon! Awesome!';
+        } else if (diffInDays > 1) {
+            sidebar.innerText = `You last visited ${diffInDays} days ago.`
+        } else {
+            sidebar.innerText = `You last visited 1 day ago.`
+        }
+    }
+
+    localStorage.setItem("lastVisit", currentDate.toString());
+}
+
+window.addEventListener("load", updateSidebarMessage);
